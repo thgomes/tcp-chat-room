eded632d11eeeb61cc10f90530656e98c6d04068
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-06-24 12:08:10 -0300

    chore: add client and server structs

---
+++ client.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <netinet/in.h>
+
+typedef struct {
+    struct sockaddr_in addr;
+} Server;
+
+int main () {
+    printf("Hello world!\n");
+}
---
+++ server.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <netinet/in.h>
+
+#define MAX_CLIENT_NAME 50
+
+typedef struct {
+    int id;
+    char name[MAX_CLIENT_NAME];
+    struct sockaddr_in addr;
+} Client;
+
+typedef struct {
+    struct sockaddr_in addr;
+} Server;
+
+int main () {
+    printf("Hello world!\n");
+}
