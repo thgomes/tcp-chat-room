b45a2dfee38c481b1a50b36619618a6fc696dcbd
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-06-26 16:42:21 -0300

    chore: add room struct

--- server.c
+++ server.c
@@ -2,6 +2,8 @@
 #include <netinet/in.h>
 
 #define MAX_CLIENT_CHAR_NAME 50
+#define MAX_ROOM_CHAR_NAME 50
+#define MAX_CLIENTS_ON 25
 
 typedef struct
 {
@@ -9,13 +11,17 @@ typedef struct
     char name[MAX_CLIENT_CHAR_NAME];
     struct sockaddr_in addr;
 } Client;
-
 typedef struct
 {
     struct sockaddr_in addr;
 } Server;
-
-int main()
+typedef struct 
 {
+    int id;
+    char name[MAX_ROOM_CHAR_NAME];
+    Client clients_on[MAX_CLIENTS_ON];
+} Room;
+
+int main () {
     printf("Hello world!\n");
 }
