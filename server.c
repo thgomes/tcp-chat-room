bd61b3f0653d36f838c50a180529b48a7239183d
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-06-26 16:32:36 -0300

    fix: add netinet include in server

--- server.c
+++ server.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <netinet/in.h>
 
-#define MAX_CLIENT_NAME 50
+
+#define MAX_CLIENT_CHAR_NAME 50
 
 typedef struct {
     int id;
-    char name[MAX_CLIENT_NAME];
-    struct sockaddr_in addr;
+    char name[MAX_CLIENT_CHAR_NAME];
+    struct sockaddr_in addr
 } Client;
-
-typedef struct {
-    struct sockaddr_in addr;
-} Server;
 
 int main () {
     printf("Hello world!\n");
